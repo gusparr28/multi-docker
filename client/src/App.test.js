52789acb17c76c62b40a58a034ff9cd1ed92f5e1
Gustavo Parra <gustavoparrabotero@gmail.com>
2023-02-06 21:24:24 -0300

    changes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,1 +1,5 @@
-test('renders without crashing', () => { });
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {});
